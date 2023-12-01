b7ca66fd9e61122e73428bc9c57aac166358e9f0
Johan Förberg <johan@forberg.se>
2023-12-01 06:15:25 +0100

    2023: Day 1

--- 2023/1/a.py
+++ 2023/1/a.py
@@ -3,9 +3,16 @@
 import numpy as np
 import sys
 
-e = []
+s = 0
 
-for l in sys.stdin:
-    pass
+for l in open('input.txt'):
+    v = ''
+    for c in l:
+        if c in "0123456789":
+            v += c
 
-print(e)
+    x = int(v[0] + v[-1])
+    s += x
+
+print(s)
+
--- 2023/1/b.py
+++ 2023/1/b.py
@@ -3,9 +3,35 @@
 import numpy as np
 import sys
 
-e = []
+s = 0
 
-for l in sys.stdin:
-    pass
+for l in open('input.txt'):
+    v = ''
+    for i in range(len(l)):
+        p = l[i:]
+        if p[0] in "0123456789":
+            v += p[0]
+        elif p.startswith('one'):
+            v += '1'
+        elif p.startswith('two'):
+            v += '2'
+        elif p.startswith('three'):
+            v += '3'
+        elif p.startswith('four'):
+            v += '4'
+        elif p.startswith('five'):
+            v += '5'
+        elif p.startswith('six'):
+            v += '6'
+        elif p.startswith('seven'):
+            v += '7'
+        elif p.startswith('eight'):
+            v += '8'
+        elif p.startswith('nine'):
+            v += '9'
 
-print(e)
+    x = int(v[0] + v[-1])
+    s += x
+
+print(s)
+
