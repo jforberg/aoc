86af5a430e22d4d344796273b1e928cb136d54e1
Johan Förberg <johan@forberg.se>
2024-09-01 22:21:09 +0200

    2015: Day 16

---
+++ 2015/16/b.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import numpy as np
+from collections import defaultdict as ddict
+import sys
+import re
+import itertools
+
+table = ddict(lambda: ddict(lambda: None))
+
+for l in open('input.txt'):
+    l = l.strip()
+    if not l:
+        continue
+    n = l.split(': ')[0]
+    xs = l[len(n) + 2:].split(', ')
+    for x in xs:
+        p, c = x.split(': ')
+        table[n][p] = int(c)
+
+wanted = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1,
+}
+
+for n, x in table.items():
+    c = 0
+    for p, v in wanted.items():
+        if p in ('cats', 'trees'):
+            if x[p] is not None and v < x[p]:
+                c += 1
+        elif p in ('pomeranians', 'goldfish'):
+            if x[p] is not None and v > x[p]:
+                c += 1
+        else:
+            if x[p] is not None and v == x[p]:
+                c += 1
+
+    if c == 3:
+        print(n.split(' ')[1])
+        sys.exit()
+
+# 3, 6
