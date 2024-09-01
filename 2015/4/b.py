e1584f8fabe17ce1403f907eda5b9f136f7595f6
Johan Förberg <johan@forberg.se>
2024-09-01 10:34:43 +0200

    2015: Day 4

---
+++ 2015/4/b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import numpy as np
+from collections import defaultdict
+import hashlib
+import sys
+
+base_k = 'ckczppom'
+
+for i in range(999999999999):
+    k = base_k + str(i)
+    h = hashlib.md5(k.encode('ascii')).hexdigest()
+
+    if h.startswith('000000'):
+        print(i)
+        sys.exit()
+
