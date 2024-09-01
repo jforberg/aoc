400dae9ee730f8d0f1fd5df5631789693c5f08ef
Johan Förberg <johan@forberg.se>
2024-09-01 11:16:22 +0200

    2015: Day 6

---
+++ 2015/6/a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import numpy as np
+from collections import defaultdict
+import sys
+import re
+
+g = np.zeros((1000, 1000), dtype=bool)
+s = 0
+
+for l in open('input.txt'):
+    l = l.strip()
+    if not l:
+        continue
+
+    m = re.search(r'([^0-9]+) (\d+),(\d+) through (\d+),(\d+)', l)
+    op, j0, i0, j1, i1 = m.groups()
+    j0 = int(j0)
+    j1 = int(j1)
+    i0 = int(i0)
+    i1 = int(i1)
+
+    if op == 'turn on':
+        g[i0:i1 + 1, j0:j1 + 1] = 1
+    elif op == 'toggle':
+        g[i0:i1 + 1, j0:j1 + 1] = ~g[i0:i1 + 1, j0:j1 + 1]
+    elif op == 'turn off':
+        g[i0:i1 + 1, j0:j1 + 1] = 0
+    else:
+        assert False
+
+print(np.count_nonzero(g == 1))
