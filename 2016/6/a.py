496a2bf44c86ea7a8ee0574657e833888e26f60c
Johan Förberg <johan@forberg.se>
2024-09-22 13:50:29 +0200

    2016: Day 6
    1s: 2:37
    2s: 3:02

---
+++ 2016/6/a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import numpy as np
+from collections import defaultdict as ddict
+import sys
+import re
+import itertools
+
+s = 0
+
+tab = []
+for i in range(8):
+    tab.append(ddict(int))
+
+for l in open('input.txt'):
+    l = l.strip()
+    if not l:
+        continue
+
+    for i, c in enumerate(l):
+        tab[i][c] += 1
+
+msg = ''
+for i in range(8):
+    msg += list(sorted(tab[i].items(), key=lambda x: x[1]))[-1][0]
+
+print(msg)
