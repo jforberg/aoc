835877289b8960d4a86d679c8d2d8ee5a975245c
Johan Förberg <johan@forberg.se>
2024-11-24 15:48:34 +0100

    2016: Day 16

---
+++ 2016/16/a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+l = 272
+
+init = '01000100010010111'
+
+d = init
+while len(d) < l:
+    b = d[::-1]
+    b = b.replace('0', 'a').replace('1', '0').replace('a', '1')
+    d = d + '0' + b
+
+d = d[:l]
+
+cs = d
+while True:
+    s0 = cs[::2]
+    s1 = cs[1::2]
+    cs = ''.join([ '1' if a == b else '0' for a, b in zip(s0, s1) ])
+
+    if len(cs) % 2:
+        break
+
+print(cs)
