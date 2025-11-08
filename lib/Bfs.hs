c37ba48f16002b00decdaabd6756a02cf3790e6b
Johan Förberg <johan@forberg.se>
2025-11-08 12:21:30 +0100

    lib/Bfs: Simplify API

--- lib/Bfs.hs
+++ lib/Bfs.hs
@@ -1,6 +1,6 @@
 -- | Breadth-first graph search
 module Bfs
-( Node(..)
+( Result(..)
 , bfs
 )
 where
@@ -8,28 +8,23 @@ where
 import Data.Map qualified as Map
 import Data.Sequence qualified as Seq
 
--- | Generic representation of a node in the graph
-class Ord a => Node a where
-    -- | True if this node represents the goal which terminates the search
-    isGoal :: a -> Bool
-
-    -- | A list of nodes adjacent to this node
-    adjacent :: a -> [a]
+data Result n = Goal | Adj [n]
+    deriving (Show, Eq)
 
 -- | Find the shortest path from 'start' to the goal. The path is returned in reverse order.
-bfs :: Node a => a -> [a]
-bfs start = search (Map.singleton start start) (Seq.singleton start)
+bfs :: Ord n => (n -> Result n) -> n -> [n]
+bfs rule start = search (Map.singleton start start) (Seq.singleton start)
   where
     search parents Seq.Empty = []
-    search parents (v Seq.:<| queue)
-       | isGoal v = reconstruct parents v
-       | otherwise = populate parents queue v (adjacent v)
+    search parents (v Seq.:<| queue) = case rule v of
+        Goal -> reconstruct parents v
+        Adj as -> populate parents queue v as
 
     populate parents queue v [] = search parents queue
     populate parents queue v (w:ws)
-       | w `Map.member` parents = populate parents queue v ws
-       | otherwise = populate (Map.insert w v parents) (queue Seq.:|> w) v ws
+      | w `Map.member` parents = populate parents queue v ws
+      | otherwise = populate (Map.insert w v parents) (queue Seq.:|> w) v ws
 
     reconstruct parents last
-       | last == start = [start]
-       | otherwise = let p = parents Map.! last in last : reconstruct parents p
+      | last == start = [start]
+      | otherwise = let p = parents Map.! last in last : reconstruct parents p
--- lib/TestBfs.hs
+++ lib/TestBfs.hs
@@ -2,9 +2,6 @@ import Data.Array.IArray as A
 
 import Bfs
 
-data N = N (Int, Int)
-    deriving (Show, Eq, Ord)
-
 mazeStr = "..XX.X.X\
           \X...X.XX\
           \XXX.XXXX\
@@ -17,15 +14,13 @@ mazeStr = "..XX.X.X\
 maze :: A.Array (Int, Int) Char
 maze = A.listArray ((0, 0), (7, 7)) $ concat $ lines mazeStr
 
-instance Node N where
-    isGoal (N (x, y)) = x == (8 - 1) && y == (8 - 1)
-    adjacent (N (x, y)) = [(0, 1), (0, -1), (1, 0), (-1, 0)] >>= \(dx, dy) ->
+rule (x, y)
+  | (x, y) == (7, 7) = Goal
+  | otherwise = Adj $ [(0, 1), (0, -1), (1, 0), (-1, 0)] >>= \(dx, dy) ->
         let idx = (x + dx, y + dy) in
         case maze A.!? idx of
-            Just '.' -> [N idx]
+            Just '.' -> [idx]
             Just 'X' -> []
             Nothing -> []
 
-start = N (0, 0)
-
-main = print $ bfs start
+main = print $ bfs rule (0, 0)
