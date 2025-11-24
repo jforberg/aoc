84f26ef5a711ebfe492a61646fbaaf3684c58ba5
Johan Förberg <johan@forberg.se>
2025-11-24 21:16:06 +0100

    2017: Day 6

---
+++ 2017/6/a.hs
@@ -0,0 +1,39 @@
+import System.IO.Unsafe
+import Data.List.Extra
+import Debug.Trace
+import Data.Sequence qualified as Seq
+import Data.Foldable
+import Data.Set qualified as Set
+import Data.Maybe
+
+n = 16
+
+inp = unsafePerformIO $ readFile "input.txt"
+
+banks = Seq.fromList $ (read :: String -> Int) <$> words inp
+
+iteration = go Set.empty
+  where
+    go seen bs
+      | Set.member bs seen = 0
+      | otherwise = let
+            i = maximumIdx bs
+            bs' = redist bs i
+            seen' = Set.insert bs seen in
+                1 + go seen' bs'
+
+redist bs i0 = foldr f bs' $ take r $ concat $ repeat ([i0 + 1..n - 1] <> [0..i0])
+  where
+    f i acc = Seq.adjust' (+1) i acc
+    bs' = Seq.update i0 0 bs
+    r = fromJust $ bs Seq.!? i0
+
+maximumIdx xs = fst $ foldl' f (-1, -1) $ zip [0..] $ toList xs
+  where
+    f (mi, mx) (i, x)
+      | x > mx = (i, x)
+      | otherwise = (mi, mx)
+
+solution = iteration banks
+
+main = print solution
